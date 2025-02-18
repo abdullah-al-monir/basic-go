aa742dc59c148d8da52c88ff5d663937a89a46cd
abdullah-al-monir <abdullahalmonir364@gmail.com>
2025-02-18 15:28:39 +0600

    Created a new folder Internal memory where I have explained about Data Segment, Code Segment, Stack and Heap

---
+++ internal-memory/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Data Segment (Initialized global variable)
+var a = 10
+
+// init() is executed before main(), part of the Code Segment
+func init() {
+	fmt.Println("Initializing...")
+}
+
+// add() function resides in the Code Segment
+func add(x int, y int) {
+	// Stack Segment (Local variables: x, y, z)
+	z := x + y // z is pushed onto the stack
+	fmt.Println("Sum:", z)
+} // z is popped from the stack when function exits
+
+func main() {
+	// Function calls use the Stack Segment
+	add(5, 4)   // x=5, y=4, z=9 stored in stack
+	add(a, 5)   // a is in Data Segment, y=5 in stack
+
+	// Heap Segment (Demonstrating heap allocation)
+	p := new(int) // p is a pointer allocated on heap
+	*p = 20       // Assigning value to heap memory
+	fmt.Println("Heap allocated value:", *p) // Accessing heap memory
+} // p is deallocated by Go’s garbage collector
