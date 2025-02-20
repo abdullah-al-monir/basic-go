6e55771afbdb95adf8809fedc87926e2dbf579dd
abdullah-al-monir <abdullahalmonir364@gmail.com>
2025-02-20 06:53:19 +0600

    Updated internal memory folder

--- internal-memory/main.go
+++ internal-memory/main.go
@@ -21,11 +21,48 @@ func add(x int, y int) {
 
 func main() {
 	// Function calls use the Stack Segment
-	add(5, 4)   // x=5, y=4, z=9 stored in stack
-	add(a, 5)   // a is in Data Segment, y=5 in stack
+	add(5, 4) // x=5, y=4, z=9 stored in stack
+	add(a, 5) // a is in Data Segment, y=5 in stack
 
 	// Heap Segment (Demonstrating heap allocation)
-	p := new(int) // p is a pointer allocated on heap
-	*p = 20       // Assigning value to heap memory
+	p := new(int)                            // p is a pointer allocated on heap
+	*p = 20                                  // Assigning value to heap memory
 	fmt.Println("Heap allocated value:", *p) // Accessing heap memory
 } // p is deallocated by Go’s garbage collector
+
+/*
+  2 Phases:
+	 1. Compilation Phase
+	 2. Execution Phase
+  
+	 *** Compile Phase ***
+
+	 *** Code Segment ***
+	 a = 10
+	 add = func (){...}
+	 main = func() {...}
+	 init = func() {...}
+
+
+
+	go run main.go => compile it => main => ./main
+	go build main.go => compile it => main
+
+*/
+
+/*
+ *** Binary File ***
+
+  main
+
+	0011010101101
+	0011010101101
+	0011010101101
+	0011010101101
+	0011010101101
+	0011010101101
+
+
+
+
+*/
